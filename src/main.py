ab12d5bff0355a103287f50c314e64663e9f0f02
paola9896 <paolasc2652@gmail.com>
2021-11-18 01:12:57 +0000

    updates

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Contact
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,63 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/contact', methods=['GET'])
+def get_all_contacts():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    contacts_query = Contact.query.all()
+    all_contacts = list(map(lambda x: x.serialize(), contacts_query))
+    return jsonify(all_contacts), 200
 
-    return jsonify(response_body), 200
+@app.route('/contact', methods=['POST'])
+def insert_contact():
+
+    body = request.get_json()
+
+    contact1 = Contact(full_name=body["full_name"], email=body["email"], phone=body["phone"], address=body["address"])
+    db.session.add(contact1)
+    db.session.commit()
+
+    contacts_query = Contact.query.all()
+    all_contacts = list(map(lambda x: x.serialize(), contacts_query))
+    return jsonify(all_contacts), 200
+
+@app.route('/contact/<int:id>', methods=['PUT'])
+def update_contact(id):
+
+    body = request.get_json()
+
+    contact1 = Contact.query.get(id)
+    if contact1 is None:
+        raise APIException('Contact not found', status_code=404)
+
+    #Checking is key value exists, and then reassign value
+    if "full_name" in body:
+        contact1.full_name = body["full_name"]
+    if "email" in body:
+        contact1.email = body["email"]
+    if "phone" in body:
+        contact1.phone = body["phone"]
+    if "address" in body:
+        contact1.address = body["address"]
+    db.session.commit()
+
+    contacts_query = Contact.query.all()
+    all_contacts = list(map(lambda x: x.serialize(), contacts_query))
+    return jsonify(all_contacts), 200
+
+@app.route('/contact/<int:id>', methods=['DELETE'])
+def delete_contact(id):
+
+    contact1 = Contact.query.get(id)
+    if contact1 is None:
+        raise APIException('Contact not found', status_code=404)
+    db.session.delete(contact1)
+    db.session.commit()
+
+    contacts_query = Contact.query.all()
+    all_contacts = list(map(lambda x: x.serialize(), contacts_query))
+    return jsonify(all_contacts), 200
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,23 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
+    full_name = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(80), unique=False, nullable=False)
+    phone = db.Column(db.String(80), unique=False, nullable=False)
+    address = db.Column(db.String(80), unique=False, nullable=False)
+    
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Contact %r, %r>' % self.full_name, self.email
 
     def serialize(self):
         return {
             "id": self.id,
+            "full_name": self.full_name,
             "email": self.email,
+            "phone": self.phone,
+            "address": self.address,
             # do not serialize the password, its a security breach
         }
+
